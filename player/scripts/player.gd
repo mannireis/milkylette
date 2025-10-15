d9f32002cd8dc48a4d63f0976083392a7087c4f5
Manni Reis <90857553+mannireis@users.noreply.github.com>
2025-10-15 13:39:15 +0100

    dashing works :yay: https://hc-cdn.hel1.your-objectstorage.com/s/v3/51145ed043852c1de2f39c910e4e174673406c69_2025-10-15_13-35-01.mp4

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -5,8 +5,8 @@ const ACCELARATION : float = 16.5
 const FRICTION : float = 8.5
 
 const DASH_RELOAD_COST : float = 0.5
-const DASH_SPEED : float = 180
-const DASH_TIME : float = 0.12
+const DASH_SPEED : float = 300
+const DASH_TIME : float = 0.2
 var can_dash : bool = true
 var dash_timer : float = 0
 var dash_dir : Vector2 = Vector2.ZERO
@@ -14,26 +14,26 @@ var dash_reload_timer : float = 0
 
 
 func _physics_process(delta: float) -> void:
-	if dash_timer == 0:
-		var input : Vector2 = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), 
-		Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
-		
-		var velocity_weight_x : float = 1 - exp( -(ACCELARATION if input.x else FRICTION) * delta)
-		velocity.x = lerp(velocity.x, input.x * MAX_SPEED, velocity_weight_x)
-		
-		var velocity_weight_y : float = 1 - exp( -(ACCELARATION if input.y else FRICTION) * delta)
-		velocity.y = lerp(velocity.y, input.y * MAX_SPEED, velocity_weight_y)
 	
-	dash_logic(delta)
+	var input : Vector2 = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), 
+	Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
+		
+	var velocity_weight_x : float = 1 - exp( -(ACCELARATION if input.x else FRICTION) * delta)
+	velocity.x = lerp(velocity.x, input.x * MAX_SPEED, velocity_weight_x)
+		
+	var velocity_weight_y : float = 1 - exp( -(ACCELARATION if input.y else FRICTION) * delta)
+	velocity.y = lerp(velocity.y, input.y * MAX_SPEED, velocity_weight_y)
+	
+	dash_logic(delta, input)
 	move_and_slide()
 
-func dash_logic(delta: float) -> void:
+func dash_logic(delta: float, input: Vector2) -> void:
 	if can_dash and Input.is_action_just_pressed("dash"):
 		can_dash = false
 		dash_timer = DASH_TIME
 		dash_reload_timer = DASH_RELOAD_COST
 		
-		dash_dir = global_position.direction_to(get_global_mouse_position())
+		dash_dir = input
 		velocity = dash_dir * DASH_SPEED
 	
 	if dash_timer > 0:
